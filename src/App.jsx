f6672c37d517cb532ea47518bc78c9681d986c58
joearias1121 <joearias1121@gmail.com>
2025-03-12 23:42:37 -0400

    added search feature

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,13 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -51,8 +53,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
@@ -80,7 +82,6 @@ const App = () => {
             </ul>
           )}
         </section>
-        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   );
